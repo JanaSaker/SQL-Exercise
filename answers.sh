d789f48dfa57106d5f3d9dda09b00ce71852b7d1
JanaSaker <janasakr2000@gmail.com>
2023-09-19 06:59:18 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -12,3 +12,25 @@ VALUES ('Janaa',23, 'F', 0);
 UPDATE students SET Points = Points + 20 WHERE ID = 2;
 
 UPDATE students SET Points = Points - 10 WHERE ID = 1;
+
+---------Create table
+
+CREATE TABLE Graduate (
+    gradID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    gradDate TEXT
+);
+
+---------- Move data from 'students' to 'graduate' 
+
+INSERT INTO graduates (Name, Age, Gender, Points, gradDate)
+SELECT Name, Age, Gender, Points, '08/09/2018' AS gradDate
+FROM students
+WHERE Name = 'Layal';
+
+---------- Remove Layal's record from 'students'
+DELETE FROM students WHERE Name = 'Layal';
+
